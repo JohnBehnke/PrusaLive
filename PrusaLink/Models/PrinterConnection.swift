998e45d21d027972e5794e88099b576cbaa873f0
John Behnke <john@behnke.xyz>
2024-12-12 14:38:56 -0500

    Reorganize, add PrinterConnection model

---
+++ PrusaLink/Models/PrinterConnection.swift
@@ -0,0 +1,24 @@
+//
+//  PrinterConnection.swift
+//  PrusaLink
+//
+//  Created by John Behnke on 12/12/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class PrinterConnection: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var ipAddress: String
+    var apiKey: String
+    
+    
+    init(name: String, ipAddress: String, apiKey: String) {
+        self.name = name
+        self.ipAddress = ipAddress
+        self.apiKey = apiKey
+    }
+}
--- PrusaLink/PrusaLinkApp.swift
+++ PrusaLink/PrusaLinkApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct PrusaLinkApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            PrinterConnection.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
@@ -25,7 +25,7 @@ struct PrusaLinkApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PrinterListView()
         }
         .modelContainer(sharedModelContainer)
     }
